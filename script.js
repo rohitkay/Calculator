dd274852a10f559bdfccc5a5ed951a099207ef66
Rohit <192195109+rohitkay@users.noreply.github.com>
2026-01-08 21:14:16 +1100

    Get clear all functionality and addition with two numbers working

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(a,b){
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a,b){
@@ -21,6 +21,7 @@ let operator = null;
 function operate(operatorValue,number1,number2){
     if (operatorValue == "+"){
         let result = add(number1,number2);
+        updateScreen(result);
     }
     if (operatorValue == "-"){
         let result = subtract(number1,number2);
@@ -33,17 +34,72 @@ function operate(operatorValue,number1,number2){
     }
 }
 
-const digit1 = document.querySelector("#digit1");
-digit1.addEventListener("click",pushNum1(1));
+
+let calculatorState ={
+    display: "0",
+    firstOperand: null,
+    secondOperand: null,
+    operator: null,
+    timeForSecond: false,
+
+}
+
+const digitButtons = document.querySelectorAll(".digitButton");
+digitButtons.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        //console.log(button.textContent);
+        if (calculatorState.timeForSecond == false){
+            pushNum1(button.textContent);
+        }
+        else{
+            pushNum2(button.textContent);
+        }
+
+    })
+});
+
+const clearAllButton = document.querySelector("#clearAllButton");
+clearAllButton.addEventListener("click",()=>{
+    num1 = [];
+    num2 = [];
+    calculatorState.display = "0";
+    updateScreen(calculatorState.display);
+});
+
+const plusButton = document.querySelector("#addButton");
+plusButton.addEventListener("click",()=>{
+    calculatorState.firstOperand = num1;
+    calculatorState.timeForSecond = true;
+    calculatorState.operator = "+";
+});
+
+const equalButton = document.querySelector("#equalButton");
+equalButton.addEventListener("click",()=>{
+    operate(calculatorState.operator,num1.join(""),num2.join(""));
+});
+
+
+function updateScreen(value){
+    const screenLive = document.querySelector(".liveText");
+    screenLive.textContent = value;
+}
 
 function pushNum1(num){
     num1.push(num);
-    updateScreenLive(num1);
+    updateScreenLive(num1.join(""));
+    console.log(num1);
 }
+
+function pushNum2(num){
+    num2.push(num);
+    updateScreenLive(num2.join(""));
+    console.log(num2);
+}
+
 
 function updateScreenLive(liveResult){
     const screenLive = document.querySelector(".liveText");
     screenLive.textContent = liveResult;
 }
 
-console.log(num1);
+//console.log(num1);
