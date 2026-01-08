46b6799be6533c8bc016e7a2dc294da38baeea98
Rohit <192195109+rohitkay@users.noreply.github.com>
2026-01-08 19:18:23 +1100

    Add screen update when 1 is pressed

--- script.js
+++ script.js
@@ -14,6 +14,36 @@ function divide(a,b){
     return a / b;
 }
 
-let num1 = 0;
-let num2 = 0;
+let num1 = [];
+let num2 = [];
 let operator = null;
+
+function operate(operatorValue,number1,number2){
+    if (operatorValue == "+"){
+        let result = add(number1,number2);
+    }
+    if (operatorValue == "-"){
+        let result = subtract(number1,number2);
+    }
+    if (operatorValue == "×"){
+        let result = multiply(number1,number2);
+    }
+    if (operatorValue == "÷"){
+        let result = divide(number1,number2);
+    }
+}
+
+const digit1 = document.querySelector("#digit1");
+digit1.addEventListener("click",pushNum1(1));
+
+function pushNum1(num){
+    num1.push(num);
+    updateScreenLive(num1);
+}
+
+function updateScreenLive(liveResult){
+    const screenLive = document.querySelector(".liveText");
+    screenLive.textContent = liveResult;
+}
+
+console.log(num1);
