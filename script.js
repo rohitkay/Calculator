70ef2f45da665e40f4ff225ae26ec00d7064e93d
Rohit <192195109+rohitkay@users.noreply.github.com>
2026-01-08 18:33:24 +1100

    Create the basic functions for add, subtract, divide and multiply

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+let num1 = 0;
+let num2 = 0;
+let operator = null;
